ec0b4309b606c4a25fd59c219b3d1fab8782c738
Hudson Baroni <hudsonbarni@gmail.com>
2022-02-28 22:19:07 -0300

    First Class - Position

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(10, 12);
+		
+		System.out.println(pos);
+
+	}
+
+}
