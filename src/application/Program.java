316a00db3284d6334a3a9a16b66376f1f1b53313
Hudson Baroni <hudsonbarni@gmail.com>
2022-04-03 19:49:26 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import boardgame.BoardExceptions;
@@ -12,40 +14,43 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
 		Scanner reader = new Scanner(System.in);
-		try {
-			ChessMatch chessMatch = new ChessMatch();
-			while (true) {
-				try {
-					UI.clearScreen();
-					UI.printMatch(chessMatch);
-					System.out.print("Source: ");
-					ChessPosition source = UI.readChessPosition(reader);
-					
-					boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-					UI.clearScreen();
-					UI.printBoard(chessMatch.getPieces(), possibleMoves);
-					
-					System.out.println();
-					System.out.print("Target: ");
-					ChessPosition target = UI.readChessPosition(reader);
-					
-					ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(reader);
+
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(reader);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
 				}
-				catch (ChessException e) {
-					System.out.println(e.getMessage());
-					reader.nextLine();
-				}
-				catch (InputMismatchException e) {
-					System.out.println(e.getMessage());
-					reader.nextLine();
-				}
+				
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				reader.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				reader.nextLine();
+			} catch (BoardExceptions e) {
+				System.out.println(e.getMessage());
 			}
-		} catch (BoardExceptions e) {
-			System.out.println(e.getMessage());
+			
 		}
-		reader.close();
 	}
 }
